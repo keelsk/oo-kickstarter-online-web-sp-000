8526a1cc72bfe5d899ef60242b0ce8560f711e8f
Kirsten Keels <keelsk@gmail.com>
2020-03-06 21:25:04 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,6 @@
 class Project
   
-  attr_reader :title, :backers, :backer
+  attr_reader :title, :backers, :back_project
   
   def initialize(title)
     @title = title
@@ -9,6 +9,6 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.
+    backer.back_project = backer
   end
 end
